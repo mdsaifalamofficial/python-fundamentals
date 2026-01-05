6fefdf5402c55ca9cde1baa476d5087b2eee6a84
Md Saif Alam <mdsaifalamofficial@gmail.com>
2026-01-05 17:09:39 +0530

    Create loop_control_break_continue.py
    loop control using break and continue

---
+++ loop_control_break_continue.py
@@ -0,0 +1,15 @@
+# Loop Control Statements (break & continue)
+
+# break example
+for i in range(1, 6):
+    if i == 3:
+        break
+    print(i)
+
+print("----")
+
+# continue example
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
