c54fe0d7f52429cd66a244e03d6f041849d466c5
Md Saif Alam <mdsaifalamofficial@gmail.com>
2026-01-06 14:26:26 +0530

    Create functions_basic.py
    basic functions with parameters

---
+++ functions_basic.py
@@ -0,0 +1,12 @@
+# Functions in Python
+
+def greet():
+    print("Hello, welcome to Python")
+
+greet()
+
+def greet_user(name):
+    print("Hello", name)
+
+greet_user("Saif")
+greet_user("Rahul")
