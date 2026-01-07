c7ac9ce07217f83bcad64f2df1ba508be2e7966f
Md Saif Alam <mdsaifalamofficial@gmail.com>
2026-01-07 19:25:56 +0530

    Create functions_return.py
    functions using return statement

---
+++ functions_return.py
@@ -0,0 +1,13 @@
+# Functions with return statement
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print(result)
+
+def square(num):
+    return num * num
+
+ans = square(5)
+print(ans)
