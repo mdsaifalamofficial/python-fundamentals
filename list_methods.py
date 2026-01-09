12634ec82a740350519013999ad83dea44800b3a
Md Saif Alam <mdsaifalamofficial@gmail.com>
2026-01-09 18:02:49 +0530

    Create list_methods.py
    basic list methods in python

---
+++ list_methods.py
@@ -0,0 +1,35 @@
+# List methods in Python
+
+numbers = [1, 2, 3]
+
+# append
+numbers.append(4)
+print(numbers)
+
+# insert
+numbers.insert(1, 10)
+print(numbers)
+
+# remove
+numbers.remove(2)
+print(numbers)
+
+# pop
+numbers.pop(1)
+print(numbers)
+
+# count
+numbers.append(3)
+print(numbers.count(3))
+
+# sort
+numbers.sort()
+print(numbers)
+
+# reverse
+numbers.reverse()
+print(numbers)
+
+# clear
+numbers.clear()
+print(numbers)
